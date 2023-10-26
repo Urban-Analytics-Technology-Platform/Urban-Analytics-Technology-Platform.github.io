714316dc826e95a1631fbb5c2abfca17ae741542
Stuart Lynn <stuart.lynn@gmail.com>
2023-10-26 12:27:43 +0100

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://urban-analytics-technology-platform.github.io'
+  site: 'https://urban-analytics-technology-platform.github.io',
+  integrations: [tailwind()]
 });
