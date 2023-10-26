2b99883e11c55de0a48debe126cc12fa8dd64e60
Stuart Lynn <stuart.lynn@gmail.com>
2023-10-26 10:36:52 +0100

    Config for gh-pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://urban-analytics-technology-platform.github.io'
+});
