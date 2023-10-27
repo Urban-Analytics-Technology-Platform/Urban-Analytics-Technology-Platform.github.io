c6cd42184322a9975deea4bdbd83e666eec129a8
Stuart Lynn <stuart.lynn@gmail.com>
2023-10-27 12:39:23 +0100

    Removing unused script

--- src/generate_projects.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd 
-
-projects = pd.read_excel("https://thealanturininstitute-my.sharepoint.com/:x:/g/personal/gmingas_turing_ac_uk/EdPdmLMM5xhIvzALQGJIi70BMK3_bAgfIgn48n1ZHdhU6g?e=2HBHLd&nav=MTVfezAwMDAwMDAwLTAwMDEtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMH0")
-print(projects)
